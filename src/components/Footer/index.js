137ef80483ad85ae4176e98cef8e0b0456631302
erguvennisanur <86314179+erguvennisanur@users.noreply.github.com>
2022-11-16 20:37:54 +0300

    Add files via upload

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+    <footer className="info">
+	<p>Click to edit a todo</p>
+	<p>Created by Me! </p>
+	<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+</footer>
+  )
+}
+
+export default Footer;
